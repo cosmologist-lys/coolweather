980783ba552c914cb9f5ec7361976271750504d1
cosmologist-lys <lyskaka22@qq.com>
2016-12-30 13:20:57 +0800

    submit gson models

---
+++ app/src/main/java/com/cosmos/kpl/coolweather/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.cosmos.kpl.coolweather.gson;
+
+
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+}
---
+++ app/src/main/java/com/cosmos/kpl/coolweather/gson/Weather.java
@@ -0,0 +1,23 @@
+package com.cosmos.kpl.coolweather.gson;
+
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Weather {
+
+    public String status;
+
+    public GsonBasic basic;
+
+    public AQI aqi;
+
+    public Now now;
+
+    public Suggestion suggestion;
+
+    @SerializedName("daily_forecast")
+    public List<Forecast> forecastList;
+
+}
